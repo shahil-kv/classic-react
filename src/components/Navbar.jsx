4c25dcbc2bfd5e9eb1adab98c0089f36881629d9
shahil-leo <shahilcode@gmail.com>
2022-11-27 20:26:02 +0530

    added the initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container=styled.div`
+  height: 50px;
+  background-color: black;
+`
+function Navbar() {
+  return (
+       <Container>
+             <h1>shail </h1>
+       </Container>
+  )
+
+}
+
+export default Navbar
