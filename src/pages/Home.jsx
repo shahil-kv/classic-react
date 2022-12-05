ac39655c104cf84de0e26a728a745afa91db50e6
shahil-leo <shahilcode@gmail.com>
2022-12-05 21:21:11 +0530

    Added the category section into the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-// import Categories from '../components/Categories'
+import Categories from '../components/Categories'
 import Navbar from '../components/Navbar'
 import Slide from '../components/Slide'
-import CategoryItem from '../components/CategoryItem'
 const Home=()=> {
   return (
     <div>
     <Navbar/>
     <Slide/>
-    {/* <Categories/> */}
-    <CategoryItem/>
+    <Categories/>
+
     </div>
   )
 } 
