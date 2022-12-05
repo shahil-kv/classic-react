24ea614ad692da167a71fc397beae3ebdaf7a6b4
shahil-leo <shahilcode@gmail.com>
2022-12-05 21:22:24 +0530

    categories addded

---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import CategoryItem from "./CategoryItem";
+import { categories } from "../data";
+console.log(categories)
+
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
+
+const Categories = () => {
+  return (
+    
+    <Container>     
+      {categories.map(item => (
+        <CategoryItem item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
+
+export default Categories;
