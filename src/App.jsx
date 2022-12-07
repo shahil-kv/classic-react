b25db494ac412c369505e63e14bdfc15280e6e8f
shahil-leo <shahilcode@gmail.com>
2022-12-07 11:46:37 +0530

    Added the One products section to app.j

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Home/>;
+  return <Register/>
 };
 
 export default App;
