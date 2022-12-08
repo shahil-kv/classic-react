d628e8d28f6f328b3614a075ab71ba70081f5154
shahil-leo <shahilcode@gmail.com>
2022-12-09 00:30:09 +0530

    Added the categories new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 // import Login from "./pages/Login";
 // import Product from "./pages/Product";
 // import ProductList from "./pages/ProductList";
 // import Register from "./pages/Register";
 // import Cart from './pages/Cart';
-import Login from './pages/Login'
+// import Login from './pages/Login'
 const App = () => {
-  return <Login/>
+  return <Home/>
 };
 
 export default App;
